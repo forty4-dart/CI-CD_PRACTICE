4b2a593cc4885ba452b662d5089ab4ada635d6ee
forty dart <forty4.dart@gmail.com>
2026-09-02 16:00:20 +0100

    Revert "Add Terraform configuration files and resources for S3 bucket management"
    This reverts commit 14cdfc4e0a0637aa0b96d0b1c39ddda975f23140.

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "Name of the S3 practice bucket."
-  value       = aws_s3_bucket.practice.bucket
-}
-
-output "bucket_arn" {
-  description = "ARN of the S3 practice bucket."
-  value       = aws_s3_bucket.practice.arn
-}
